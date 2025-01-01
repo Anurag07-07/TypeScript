38bf5415ce7b0e28c1f550d0c97564bc2925d4bc
Anurag07raj <anurag09raj@gmail.com>
2025-01-01 18:59:01 +0530

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+//tsc main.ts to compile once
+//tsc main.ts -w compile again nad again
+// tsc --init
+// set rootDir to ./src
+// set outDir to ./build/js
+// use tsc -w in console 
+// add in last 
+// "include": [
+//     "src"
+//   ]
+// emitnoError will be incomment to not compile when error comes
+ 
+console.log("Hello World");
+console.log("Hey");
+let a = 12;
+let b = 6;
+let c = 2;
+let d = '56';
+console.log(a / b);
+console.log(c * b);
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+//tsc main.ts to compile once
+//tsc main.ts -w compile again nad again
+// tsc --init
+// set rootDir to ./src
+// set outDir to ./build/js
+// use tsc -w in console 
+// add in last 
+// "include": [
+//     "src"
+//   ]
+
+console.log("Hello World");
+console.log("Hey");
+
+let a:number = 12
+let b:number = 6
+let c:number = 2;
+let d:string = '56';
+
+console.log(a/b);
+console.log(c*b);
+
+
